74cd460b7e6fd630f50b002340252b32e03908c2
snkolev18 <SNKolev@codingburgas.bg>
2021-02-13 17:24:01 +0200

    Implement email validation
    * Needs improvement and to be added to functions that input data

--- SchoolManagement/checker.cpp
+++ SchoolManagement/checker.cpp
@@ -51,3 +51,60 @@ bool checkIfRoleIsWhiteListed(vector<string> whiteListedRoles, string role)
 	}
 	return false;
 }
+
+bool checkEmailValidity(string email)
+{
+    size_t atPos = email.find('@');
+    size_t dotPos;
+    string username, domain;
+    string invalid = "!#$%&'*+""~\\/=?^`{|^/}[],;:";
+
+    bool invalidCharsCheck = true;
+    bool subdomainCheck = false;
+
+    if (atPos != string::npos)
+    {
+        username = email.substr(0, atPos);
+        domain = email.substr(atPos + 1);
+
+        if (isalpha(username[0]))
+        {
+            for (int i = 1; i < username.size(); i++)
+            {
+                for (size_t j = 0; j < invalid.size(); j++)
+                {
+                    if (username[i] == invalid[j])
+                    {
+                        invalidCharsCheck = false;
+                        break;
+                    }
+                }
+            }
+            dotPos = email.find('.', atPos + 2);
+
+            /*for (int i = 0; i < domain.size(); i++)
+            {
+                for (int j = 0; j < invalid.size(); j++)
+                {
+                    if(domain[i] != invalid[j])
+                    {
+                        valid++;
+                    }
+                }
+            }*/
+
+            if (dotPos != string::npos)
+            {
+                subdomainCheck = true;
+            }
+            else
+            {
+                subdomainCheck = false;
+            }
+        }
+    }
+
+    return invalidCharsCheck and subdomainCheck ? true : false;
+
+    //size_t dotPos = email.find()
+}
