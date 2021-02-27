2421a6b3c45591070e23ab22bbb690176d36ca6b
snkolev18 <SNKolev@codingburgas.bg>
2021-02-27 22:25:51 +0200

    Update setup.sh
    * Add menu
    
    -> Might change it with dialog menu package option

--- setup.sh
+++ setup.sh
@@ -1,9 +1,54 @@
 #!/bin/bash
-sudo apt-get update
-cd SchoolManagement/
-chmod +x SchoolManagement.cpp
-# Makefile is currently not working
-sudo make
-chmod +x SchoolManagement
-./SchoolManagement
+#sudo apt-get install dialog
+$(LANG=da_DK.UTF-8)
+ 
+BLUE='\033[0;34m'
+ 
+show_logo(){
+    printf "${BLUE}████████╗███████╗ █████╗ ███╗   ███╗███████╗ ██████╗ ███╗   ██╗██████╗ ██╗   ██╗██████╗  ██████╗ ███████╗████████╗\n"
+    printf "${BLUE}╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██╔════╝██╔═══██╗████╗  ██║██╔══██╗██║   ██║██╔══██╗██╔════╝ ██╔════╝╚══██╔══╝\n"
+    printf "${BLUE}   ██║   █████╗  ███████║██╔████╔██║███████╗██║   ██║██╔██╗ ██║██████╔╝██║   ██║██║  ██║██║  ███╗█████╗     ██║   \n"
+    printf "${BLUE}   ██║   ██╔══╝  ██╔══██║██║╚██╔╝██║╚════██║██║   ██║██║╚██╗██║██╔══██╗██║   ██║██║  ██║██║   ██║██╔══╝     ██║   \n"
+    printf "${BLUE}   ██║   ███████╗██║  ██║██║ ╚═╝ ██║███████║╚██████╔╝██║ ╚████║██████╔╝╚██████╔╝██████╔╝╚██████╔╝███████╗   ██║   \n"
+    printf "${BLUE}   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═══╝╚═════╝  ╚═════╝ ╚═════╝  ╚═════╝ ╚══════╝   ╚═╝  \n"
+}
+ 
+ 
+show_logo
+printf "${BLUE} Choose an option: \n"
+printf "${BLUE} 1) Execute the application: \n"
+printf "${BLUE} 2) Info about the developers: \n"
+printf "${BLUE} 3) Exit: \n"
+ 
+ 
+read opt
+until [ "$opt" == "3" ]; do
+      case $opt in
+        1) clear;
+            printf "Executing application";
+            sleep 1
+            echo '\n'
+            clear
+            cd SchoolManagement/
+            sudo make
+            clear
+            show_logo
+            ./SchoolManagement
+            break
+        ;;
+        2) clear;
+            printf "Developers info here";
+            break
+        ;;
+        3) clear;
+            printf "Are chupendra"
+            sleep 1
+            exit
+        ;;
+        *) clear;
+            option_picked "Pick an option from the menu";
+        ;;
+      esac
+  done
+ 
 exec bash
