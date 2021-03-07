780d7aa3db28601f4e5b99fc60dd3585c887c24f
snkolev18 <SNKolev@codingburgas.bg>
2021-03-07 02:54:10 +0200

    Perform unit testing to:
    * writeStudentsInTxt()
    # Not a good approach :()
    * checkTeamNameLength()

--- Unit Testing - QA/checkersUnitTests.cpp
+++ Unit Testing - QA/checkersUnitTests.cpp
@@ -1,8 +1,8 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-//#include "../SchoolManagement/checkers.h"
-//#include "../SchoolManagement/userInterface.h"
-//#include "../SchoolManagement/data.h"
+#include "../SchoolManagement/checkers.h"
+#include "../SchoolManagement/userInterface.h"
+#include "../SchoolManagement/data.h"
 #include <vector>
 #include <string>
 
@@ -11,165 +11,192 @@ using namespace std;
 
 namespace UnitTestingQA
 {
-	//TEST_CLASS(checkersUnitTests)
-	//{
-	//public:
-	//	
-	//	TEST_METHOD(checkEmailValidityShouldReturnTrueWhenEmailIsValid)
-	//	{
-	//		// Arrange
-	//		string email = "SNKolev18@codingburgas.bg";
+	TEST_CLASS(checkersUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(checkEmailValidityShouldReturnTrueWhenEmailIsValid)
+		{
+			// Arrange
+			string email = "SNKolev18@codingburgas.bg";
 
-	//		// Act
-	//		bool res = checkEmailValidity(email);
+			// Act
+			bool res = checkEmailValidity(email);
 
-	//		// Assert
-	//		Assert::AreEqual(true, res);
+			// Assert
+			Assert::AreEqual(true, res);
 
-	//	}
+		}
 
-	//	TEST_METHOD(checkEmailValidityShouldReturnFalseWhenEmailIsNotValid)
-	//	{
-	//		// Arrange
-	//		string email = "SNKol\"ev18@codingburgas.bg";
+		TEST_METHOD(checkEmailValidityShouldReturnFalseWhenEmailIsNotValid)
+		{
+			// Arrange
+			string email = "SNKol\"ev18@codingburgas.bg";
 
-	//		// Act
-	//		bool res = checkEmailValidity(email);
+			// Act
+			bool res = checkEmailValidity(email);
 
-	//		// Assert
-	//		Assert::AreEqual(false, res);
+			// Assert
+			Assert::AreEqual(false, res);
 
-	//	}
+		}
 
-	//	TEST_METHOD(checkEmailValidityShouldReturnFalseWhenEmailIsNotValidForNInvalidCharacters)
-	//	{
-	//		// Arrange
-	//		string email = "SNKolev18@codingburgas.bg";
-	//		string invalid = "!#$%&'*+\"~\\/=?^`{|/}[],;:";
-	//		//string invalid[26] = { "!", "#", "$", "%", "&", "\"", "*", "+","\"", "~", "\\", "/", "=", "?", "^", "`", "{", "|", "/", "}", "[", "]", ",", ";", ":","\"" };
-	//		vector<string> combs;
-	//		for (size_t i = 0; i < combs.size(); i++)
-	//		{
-	//			/*string hold;
-	//			hold += invalid[i];*/
-	//			email[5] = invalid[i];
-	//			combs.push_back(email);
-	//		}
-	//		// Act
-	//		vector<bool> checks;
-	//		for (size_t i = 0; i < combs.size(); i++)
-	//		{
-	//			checks.push_back(checkEmailValidity(combs[i]));
-	//		}
-	//		
+		TEST_METHOD(checkEmailValidityShouldReturnFalseWhenEmailIsNotValidForNInvalidCharacters)
+		{
+			// Arrange
+			string email = "SNKolev18@codingburgas.bg";
+			string invalid = "!#$%&'*+\"~\\/=?^`{|/}[],;:";
+			//string invalid[26] = { "!", "#", "$", "%", "&", "\"", "*", "+","\"", "~", "\\", "/", "=", "?", "^", "`", "{", "|", "/", "}", "[", "]", ",", ";", ":","\"" };
+			vector<string> combs;
+			for (size_t i = 0; i < combs.size(); i++)
+			{
+				/*string hold;
+				hold += invalid[i];*/
+				email[5] = invalid[i];
+				combs.push_back(email);
+			}
+			// Act
+			vector<bool> checks;
+			for (size_t i = 0; i < combs.size(); i++)
+			{
+				checks.push_back(checkEmailValidity(combs[i]));
+			}
+			
 
-	//		// Assert
-	//		for (int i = 0; i < checks.size(); i++)
-	//		{
-	//			Assert::IsFalse(checks[i]);
+			// Assert
+			for (int i = 0; i < checks.size(); i++)
+			{
+				Assert::IsFalse(checks[i]);
 
-	//		}
+			}
 
-	//	}
+		}
 
-	//	TEST_METHOD(checkIfRoleIsWhiteListedShouldReturnTrueIfTheRoleExistInWhitelistingVector)
-	//	{
-	//		// Arrange
-	//		string role = "Scrum Master";
-	//		vector<string> whiteList = {  };
+		TEST_METHOD(checkIfRoleIsWhiteListedShouldReturnTrueIfTheRoleExistInWhitelistingVector)
+		{
+			// Arrange
+			string role = "Scrum Master";
+			vector<string> whiteList = {  };
 
-	//		// Act
-	//		bool res = checkIfRoleIsWhiteListed(whiteList, role);
+			// Act
+			bool res = checkIfRoleIsWhiteListed(whiteList, role);
 
-	//		// Assert
-	//		Assert::AreEqual(true, res);
+			// Assert
+			Assert::AreEqual(true, res);
 
-	//	}
+		}
 
-	//	TEST_METHOD(shouldReturnTrueIfThereIsAlreadyAStudentWithGivenEmail)
-	//	{
-	//		// Arrange
-	//		vector<STUDENT> students;
-	//		students.push_back({ "Jekata", "Velikov", "10A", "DMKalchev18@codingburgas.bg" });
-	//		students.push_back({ "Ili qm", "Ili ne", "10A", "ILIQM18@codingburgas.bg" });
-	//		students.push_back({ "Stoyan", "Kolev", "10A", "SNKolev18@codingburgas.bg" });
-	//		students.push_back({ "Life", "Georgiev", "10A", "GKGeorgiev@codingburgas.bg" });
+		TEST_METHOD(shouldReturnTrueIfThereIsAlreadyAStudentWithGivenEmail)
+		{
+			// Arrange
+			vector<STUDENT> students;
+			students.push_back({ "Jekata", "Velikov", "10A", "DMKalchev18@codingburgas.bg" });
+			students.push_back({ "Ili qm", "Ili ne", "10A", "ILIQM18@codingburgas.bg" });
+			students.push_back({ "Stoyan", "Kolev", "10A", "SNKolev18@codingburgas.bg" });
+			students.push_back({ "Life", "Georgiev", "10A", "GKGeorgiev@codingburgas.bg" });
 
-	//		string tEmail = "SNKolev18@codingburgas.bg";
-	//		
+			string tEmail = "SNKolev18@codingburgas.bg";
+			
 
-	//		// Act
-	//		bool checker = checkForExistingEmailStudents(students, tEmail);
+			// Act
+			bool checker = checkForExistingEmailStudents(students, tEmail);
 
-	//		// Assert
-	//		Assert::AreEqual(true, checker, L"There is a student with that email");
+			// Assert
+			Assert::AreEqual(true, checker, L"There is a student with that email");
 
-	//	}
+		}
 
-	//	TEST_METHOD(shouldReturnFalseIfThereIsNoStudentWithGivenEmail)
-	//	{
-	//		// Arrange
-	//		vector<STUDENT> students;
-	//		students.push_back({ "Jekata", "Velikov", "10A", "DMKalchev18@test.test" });
-	//		students.push_back({ "Ili qm", "Ili ne", "10A", "ILIQM18@test.test" });
-	//		students.push_back({ "Stoyan", "Kolev", "10A", "SNKolev18@c@test.test" });
-	//		students.push_back({ "Life", "Georgiev", "10A", "GKGeorgiev@test.test" });
+		TEST_METHOD(shouldReturnFalseIfThereIsNoStudentWithGivenEmail)
+		{
+			// Arrange
+			vector<STUDENT> students;
+			students.push_back({ "Jekata", "Velikov", "10A", "DMKalchev18@test.test" });
+			students.push_back({ "Ili qm", "Ili ne", "10A", "ILIQM18@test.test" });
+			students.push_back({ "Stoyan", "Kolev", "10A", "SNKolev18@c@test.test" });
+			students.push_back({ "Life", "Georgiev", "10A", "GKGeorgiev@test.test" });
 
-	//		string sEmail = "test@test.test";
-
-
-	//		// Act
-	//		bool checker = checkForExistingEmailStudents(students, sEmail);
-
-	//		// Assert
-	//		Assert::AreEqual(false, checker, L"There is NO student with that email");
-
-	//	}
-
-	//	TEST_METHOD(shouldReturnFalseIfThereIsNoTeacherWithGivenEmail)
-	//	{
-	//		// Arrange
-	//		vector<TEACHER> teachers;
-	//		teachers.push_back({ "Teacher1", "Familiq", {"Team1", "Team2"}, "TeacherEmail1@teacher.t" });
-	//		teachers.push_back({ "Teacher2", "Familiq", {"Team1", "Team2"}, "TeacherEmail2@teacher.t" });
-	//		teachers.push_back({ "Teacher3", "Familiq", {"Team1", "Team2"}, "TeacherEmail3@teacher.t" });
-	//		teachers.push_back({ "Teacher4", "Familiq", {"Team1", "Team2"}, "TeacherEmail4@teacher.t" });
-	//		
-	//		string tEmail = "TeacherEmail5@teacher.t";
+			string sEmail = "test@test.test";
 
 
-	//		// Act
-	//		bool checker = checkForExistingEmailTeachers(teachers, tEmail);
+			// Act
+			bool checker = checkForExistingEmailStudents(students, sEmail);
 
-	//		// Assert
-	//		Assert::AreEqual(false, checker, L"There is NO teacher with that email");
+			// Assert
+			Assert::AreEqual(false, checker, L"There is NO student with that email");
 
-	//	}
+		}
 
-	//	TEST_METHOD(shouldReturnTrueIfGivenNameIsVALID)
-	//	{
-	//		// Arrange
-	//		string firstname = "Ivan";
+		TEST_METHOD(shouldReturnFalseIfThereIsNoTeacherWithGivenEmail)
+		{
+			// Arrange
+			vector<TEACHER> teachers;
+			teachers.push_back({ "Teacher1", "Familiq", {"Team1", "Team2"}, "TeacherEmail1@teacher.t" });
+			teachers.push_back({ "Teacher2", "Familiq", {"Team1", "Team2"}, "TeacherEmail2@teacher.t" });
+			teachers.push_back({ "Teacher3", "Familiq", {"Team1", "Team2"}, "TeacherEmail3@teacher.t" });
+			teachers.push_back({ "Teacher4", "Familiq", {"Team1", "Team2"}, "TeacherEmail4@teacher.t" });
+			
+			string tEmail = "TeacherEmail5@teacher.t";
 
-	//		// Act
-	//		bool isValid = checkNameValidity(firstname);
 
-	//		// Assert
-	//		Assert::IsTrue(isValid);
+			// Act
+			bool checker = checkForExistingEmailTeachers(teachers, tEmail);
 
-	//	}
+			// Assert
+			Assert::AreEqual(false, checker, L"There is NO teacher with that email");
 
-	//	TEST_METHOD(shouldReturnFalseIfGivenNameIsINVALID)
-	//	{
-	//		// Arrange
-	//		string firstname = "Ivan Georgiev";
+		}
 
-	//		// Act
-	//		bool isValid = checkNameValidity(firstname);
+		TEST_METHOD(shouldReturnTrueIfGivenNameIsVALID)
+		{
+			// Arrange
+			string firstname = "Ivan";
 
-	//		// Assert
-	//		Assert::IsTrue(isValid);
+			// Act
+			bool isValid = checkNameValidity(firstname);
 
-	//	}
-	//};
+			// Assert
+			Assert::IsTrue(isValid);
+
+		}
+
+		TEST_METHOD(shouldReturnFalseIfGivenNameIsINVALID)
+		{
+			// Arrange
+			string firstname = "Ivan Georgiev";
+
+			// Act
+			bool isValid = checkNameValidity(firstname);
+
+			// Assert
+			Assert::IsTrue(isValid);
+
+		}
+
+		TEST_METHOD(shouldReturnFalseIfTeamNameLengthIsNotCorrect)
+		{
+			// Arrange
+			string teamName = "POGOLQMOOT25KATODULJINADADADADADA";
+
+			// Act
+			bool isValid = checkTeamNameLength(teamName);
+
+			// Assert
+			Assert::IsFalse(isValid);
+
+		}
+
+		TEST_METHOD(shouldReturnTrueIfTeamNameLengthIsCorrect)
+		{
+			// Arrange
+			string teamName = "Offsec";
+
+			// Act
+			bool isValid = checkTeamNameLength(teamName);
+
+			// Assert
+			Assert::IsTrue(isValid);
+
+		}
+
+	};
 }
--- Unit Testing - QA/dataUnitTests.cpp
+++ Unit Testing - QA/dataUnitTests.cpp
@@ -1,31 +1,75 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-//#include "../SchoolManagement/checkers.h"
-//#include "../SchoolManagement/userInterface.h"
-//#include "../SchoolManagement/data.h"
+#include "../SchoolManagement/checkers.h"
+#include "../SchoolManagement/userInterface.h"
+#include "../SchoolManagement/data.h"
 #include <vector>
 #include <string>
+#include <fstream>
+
+#pragma warning
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
 
+#define TEST_DIRECTORY getCurrentDirectory(__FILE__);
+
+string getCurrentDirectory(string path) 
+{
+	const size_t last_slash_idx = path.rfind('\\');
+	if (std::string::npos != last_slash_idx)
+	{
+		return path.substr(0, last_slash_idx);
+	}
+	return "";
+}
+
 namespace UnitTestingQA
 {
-	//TEST_CLASS(checkersUnitTests)
-	//{
-	//public:
+	TEST_CLASS(checkersUnitTests)
+	{
+	public:
 
-	//	TEST_METHOD(ExpectToThrowAnExceptionIfThereIsNoDataInStudentsFile)
-	//	{
-	//		// Arrange
-	//		
+		TEST_METHOD(ExpectToThrowAnExceptionIfThereIsNoDataInStudentsFile)
+		{
+			// Arrange
+			vector<STUDENT> students =
+			{
+				{"Stoyan", "Kolev", "10A", "SNKolev18@codingburgas.bg" },
+				{"Alex", "Dinev", "10V", "AGDinev18@codingburgas.bg"},
+				{"Atanas", "Burmov", "10B", "AABurmov18@codingburgas.bg"}
+			};
+			string expect = "Stoyan|Kolev|10A|SNKolev18@codingburgas.bg|Alex|Dinev|10V|AGDinev18@codingburgas.bg|Atanas|Burmov|10B|AABurmov18@codingburgas.bg|";
+			string filename = (std::string)TEST_DIRECTORY;
+			string outer = filename;
+			
+			outer += "\\students.txt";
 
-	//		// Act
-	//		
+			// Act
 
-	//		// Assert
-	//		
+			//writeStudentsInTxt(students);
+			ofstream file(outer, ios::out);
+			if (file.is_open())
+			{
+				for (size_t i = 0; i < students.size(); i++)
+				{
+					file << students[i].delimitInfo() << endl;
+				}
+			}
+			string lines, out;
+		
+			ifstream fileOut(outer, ios::in);
+			if (fileOut.is_open()) 
+			{
+				while (!fileOut.eof())
+				{
+					getline(fileOut, lines);
+					out += lines;
+				}
+			}
+			// Assert
+			Assert::AreEqual(expect, out);
 
-	//	}
-	//};
+		}
+	};
 }
