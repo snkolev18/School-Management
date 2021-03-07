0f9270d5fd56fb87c6a509e293d3ff4272d0263a
snkolev18 <SNKolev@codingburgas.bg>
2021-03-07 13:13:32 +0200

    Perform unit testing
    * Close #25
    * Close #30

--- Unit Testing - QA/dataUnitTests.cpp
+++ Unit Testing - QA/dataUnitTests.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 #define TEST_DIRECTORY getCurrentDirectory(__FILE__);
 
-string getCurrentDirectory(string path) 
+string getCurrentDirectory(string path)
 {
 	const size_t last_slash_idx = path.rfind('\\');
 	if (std::string::npos != last_slash_idx)
@@ -30,7 +30,11 @@ namespace UnitTestingQA
 	{
 	public:
 
-		TEST_METHOD(ExpectToThrowAnExceptionIfThereIsNoDataInStudentsFile)
+		BEGIN_TEST_METHOD_ATTRIBUTE(ShouldSuccessfullyWriteDataInStudentsTxtFile)
+			TEST_OWNER(L"SNKolev18")
+			TEST_PRIORITY(1)
+			END_TEST_METHOD_ATTRIBUTE()
+			TEST_METHOD(ShouldSuccessfullyWriteDataInStudentsTxtFile)
 		{
 			// Arrange
 			vector<STUDENT> students =
@@ -42,7 +46,7 @@ namespace UnitTestingQA
 			string expect = "Stoyan|Kolev|10A|SNKolev18@codingburgas.bg|Alex|Dinev|10V|AGDinev18@codingburgas.bg|Atanas|Burmov|10B|AABurmov18@codingburgas.bg|";
 			string filename = (std::string)TEST_DIRECTORY;
 			string outer = filename;
-			
+
 			outer += "\\students.txt";
 
 			// Act
@@ -57,9 +61,9 @@ namespace UnitTestingQA
 				}
 			}
 			string lines, out;
-		
+
 			ifstream fileOut(outer, ios::in);
-			if (fileOut.is_open()) 
+			if (fileOut.is_open())
 			{
 				while (!fileOut.eof())
 				{
@@ -70,6 +74,136 @@ namespace UnitTestingQA
 			// Assert
 			Assert::AreEqual(expect, out);
 
+		}
+
+		BEGIN_TEST_METHOD_ATTRIBUTE(ShouldReturnFoundStudentByEmail)
+			TEST_OWNER(L"SNKolev18")
+			TEST_PRIORITY(1)
+			END_TEST_METHOD_ATTRIBUTE()
+			TEST_METHOD(ShouldReturnFoundStudentByEmail)
+		{
+			// Arrange
+			vector<STUDENT> students =
+			{
+				{"Stoyan", "Kolev", "10A", "SNKolev18@codingburgas.bg" },
+				{"Alex", "Dinev", "10V", "AGDinev18@codingburgas.bg"},
+				{"Atanas", "Burmov", "10B", "AABurmov18@codingburgas.bg"}
+			};
+
+
+			// Act
+			STUDENT found = findStudentByEmail(students, (string)"AGDinev18@codingburgas.bg");
+			// Assert
+
+			for (size_t i = 0; i < students.size(); i++)
+			{
+				if (students[i].email == found.email)
+				{
+					Assert::AreEqual(students[i].name, found.name);
+					Assert::AreEqual(students[i].surname, found.surname);
+					Assert::AreEqual(students[i].grade, found.grade);
+				}
+			}
+
+		}
+
+		BEGIN_TEST_METHOD_ATTRIBUTE(ShouldFailTheSearchOfAStudent)
+			TEST_OWNER(L"SNKolev18")
+			TEST_PRIORITY(1)
+			END_TEST_METHOD_ATTRIBUTE()
+			TEST_METHOD(ShouldFailTheSearchOfAStudent)
+		{
+			// Arrange
+			vector<STUDENT> students =
+			{
+				{"Stoyan", "Kolev", "10A", "SNKolev18@codingburgas.bg" },
+				{"Alex", "Dinev", "10V", "AGDinev18@codingburgas.bg"},
+				{"Atanas", "Burmov", "10B", "AABurmov18@codingburgas.bg"}
+			};
+
+
+			// Act
+			STUDENT found = findStudentByEmail(students, (string)"AGDinev@codingburgas.bg");
+			// Assert
+
+			for (size_t i = 0; i < students.size(); i++)
+			{
+				if (students[i].email == found.email)
+				{
+					Assert::AreEqual(students[i].name, found.name);
+					Assert::AreEqual(students[i].surname, found.surname);
+					Assert::AreEqual(students[i].grade, found.grade);
+				}
+				else
+				{
+					Assert::Fail(L"Couldn't find such student");
+				}
+			}
+
+		}
+
+		BEGIN_TEST_METHOD_ATTRIBUTE(ExpectToThrowAnExceptionWhenThereAreNoStudentsWhenPassingStudentsAsAnEmptyVector)
+			TEST_OWNER(L"SNKolev18")
+			TEST_PRIORITY(1)
+			END_TEST_METHOD_ATTRIBUTE()
+			TEST_METHOD(ExpectToThrowAnExceptionWhenThereAreNoStudentsWhenPassingStudentsAsAnEmptyVector)
+		{
+			// Arrange
+			vector<TEACHER> teachers = { {"Petar", "Edi koi si", {""},  "mail@mail.com"} };
+			vector<STUDENT> students;
+			vector<TEAM> teams;
+			vector<string> whiteListedRoles = { "Scrum", "QA", "Backend", "Frontend" };
+
+
+			// Act
+			try
+			{
+				teams.push_back(inputTeam(whiteListedRoles, students, teachers, teams));
+			}
+			catch (const std::runtime_error& ex)
+			{
+				if (strcmp(ex.what(), "There are no students to add in a team") == 0)
+					return;
+				Assert::Fail(L"Unexpected exception text thrown");
+			}
+			catch (...) {
+				Assert::Fail(L"Unexpected exception type thrown");
+			}
+
+			Assert::Fail(L"No exceptions were thrown");
+
+		}
+
+		BEGIN_TEST_METHOD_ATTRIBUTE(ExpectToThrowAnExceptionWhenThereAreNoTeachersWhenPassingTeachersAsAnEmptyVector)
+			TEST_OWNER(L"SNKolev18")
+			TEST_PRIORITY(1)
+			END_TEST_METHOD_ATTRIBUTE()
+			TEST_METHOD(ExpectToThrowAnExceptionWhenThereAreNoTeachersWhenPassingTeachersAsAnEmptyVector)
+		{
+			// Arrange
+			vector<TEACHER> teachers;
+			vector<STUDENT> students = { {"Georgi", "Bokluka", "10V", "qkiq123@abv.bg"}, {"Manqka za", "Znaete kakvo", "10A", "manqka928@gmail.com"} };
+			vector<TEAM> teams;
+			vector<string> whiteListedRoles = { "Scrum", "QA", "Backend", "Frontend" };
+
+
+			// Act
+			try
+			{
+				teams.push_back(inputTeam(whiteListedRoles, students, teachers, teams));
+			}
+			catch (const std::runtime_error& ex)
+			{
+				if (strcmp(ex.what(), "There are no teachers to add in a team") == 0)
+					return;
+				Assert::Fail(L"Unexpected exception text thrown");
+			}
+			catch (...) {
+				Assert::Fail(L"Unexpected exception type thrown");
+			}
+
+			Assert::Fail(L"No exceptions were thrown");
+			//Assert
 		}
 	};
 }
